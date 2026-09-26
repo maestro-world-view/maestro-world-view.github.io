d2d24938f6ecaec3882d4340e8eb5854e9cd6735
velvetroyale <rg.salesnetwork@gmail.com>
2026-09-25 21:42:53 -0400

    Maestro World View V17.4 automatic update

--- maestro_realtime.js
+++ maestro_realtime.js
@@ -6,7 +6,8 @@
  window.MAESTRO_PLACE=place;
  const bar=document.createElement('div');
  bar.className='mw-brandbar';
- bar.innerHTML='<div class="mw-head-line mw-head-line-1"><a class="mw-live-brand" href="index.html"><img src="logo.png" alt="Maestro World View"><div class="mw-logo"><span>MAESTRO WORLD VIEW</span></div></a><div class="mw-focus">'+(place?'FOCUS: '+place.toUpperCase():'GLOBAL INTELLIGENCE')+'</div></div><div class="mw-head-line mw-head-line-2"><div class="mw-byline"><em>Brought to you by Maestro World View</em></div><div class="mw-status">● LIVE · 5 MIN</div></div>';
+ var isHome=/\/(?:index\.html)?$/.test(location.pathname)||location.pathname==='/' ;
+ bar.innerHTML='<div class="mw-head-line mw-head-line-1"><a class="mw-live-brand" href="index.html"><img src="logo.png" alt="Maestro World View"><div class="mw-logo"><span>MAESTRO WORLD VIEW</span></div></a><div class="mw-head-actions"><div class="mw-focus">'+(place?'FOCUS: '+place.toUpperCase():'GLOBAL INTELLIGENCE')+'</div>'+(isHome?'':'<a class="mw-header-home" href="index.html">HOME</a>')+'</div></div><div class="mw-head-line mw-head-line-2"><div class="mw-byline"><em>Brought to you by Maestro World View</em></div><div class="mw-status">● LIVE · 5 MIN</div></div>';
  document.body.insertBefore(bar,document.body.firstChild);
  const ask=document.querySelector('.mw-ask-maestro'); if(ask) bar.insertAdjacentElement('afterend',ask);
  const foot=document.createElement('div');foot.className='mw-credit';foot.innerHTML='BROUGHT TO YOU BY <strong>MAESTRO WORLD VIEW</strong>';document.body.appendChild(foot);
