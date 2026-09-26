26b4f6d225923e2f6502b34700b1b4f538bc9f63
velvetroyale <rg.salesnetwork@gmail.com>
2026-09-25 21:16:10 -0400

    Maestro World View V17.4 automatic update

--- maestro_realtime.js
+++ maestro_realtime.js
@@ -6,8 +6,9 @@
  window.MAESTRO_PLACE=place;
  const bar=document.createElement('div');
  bar.className='mw-brandbar';
- bar.innerHTML='<a class="mw-live-brand" href="index.html"><img src="logo.png" alt="Maestro World View"><div class="mw-logo">MAESTRO <span>WORLD VIEW</span></div></a><div class="mw-focus">'+(place?'FOCUS: '+place.toUpperCase():'GLOBAL INTELLIGENCE')+'</div><div class="mw-status">● LIVE · 30 SEC</div>';
+ bar.innerHTML='<a class="mw-live-brand" href="index.html"><img src="logo.png" alt="Maestro World View"><div class="mw-logo"><span>MAESTRO WORLD VIEW</span></div></a><div class="mw-focus">'+(place?'FOCUS: '+place.toUpperCase():'GLOBAL INTELLIGENCE')+'</div><div class="mw-status">● LIVE · 5 MIN</div>';
  document.body.insertBefore(bar,document.body.firstChild);
+ const ask=document.querySelector('.mw-ask-maestro'); if(ask) bar.insertAdjacentElement('afterend',ask);
  const foot=document.createElement('div');foot.className='mw-credit';foot.innerHTML='BROUGHT TO YOU BY <strong>MAESTRO WORLD VIEW</strong>';document.body.appendChild(foot);
- setTimeout(function(){location.reload()},30000);
+ setTimeout(function(){location.reload()},300000);
 })();
